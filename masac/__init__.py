ced62059ea076924363e64c7eac26c2c9566ef61
Nat Salt <nat@nat-salt.com>
2025-04-23 12:45:24 +0100

    Add new implementation of MASAC algorithm with replay buffer and utility functions

---
+++ masac/__init__.py
@@ -0,0 +1,5 @@
+"""This package contains the implementation of the Multi-Agent SAC (MASAC) algorithm."""
+
+from masac import ma_buffer
+from masac.ma_buffer import Experience, MAReplayBuffer
+from masac.utils import extract_agent_id
